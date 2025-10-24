96e4d30a348b342adc8f229b4ba91d16448cf3e8
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-10-24 12:58:20 +0200

    added api url

--- frontend/api/api.mjs
+++ frontend/api/api.mjs
@@ -1,12 +1,12 @@
-// const API_URL = "https://js24-todo-api.onrender.com/api/todo";
+const API_URL = "https://api-image-d4kg.onrender.com/api/todo";
 
 export async function getTodos() {
-  const response = await fetch("");
+  const response = await fetch(API_URL);
   return await response.json();
 }
 
 export async function postTodo(title) {
-  const response = await fetch("", {
+  const response = await fetch(API_URL, {
     body: JSON.stringify({ todo: title }),
     method: "POST",
     headers: {
