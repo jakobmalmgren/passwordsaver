7c08f9d95564a47cf64974f8023c965f3bc35988
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-10-24 13:06:54 +0200

    added in front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
-// import { getTodos } from "../api/api.mjs";
+import { getTodos } from "../api/api.mjs";
 import AddTodo from "./components/addtodo/addtodo.jsx";
 import TodoItem from "./components/todoItems/todoItems.jsx";
 
 function App() {
-  // const todoComponents = getTodos.map((todo) => {
-  //   return <TodoItem task={todo.title} done={todo.done} key={todo.id} />;
-  // });
+  const todoComponents = getTodos.map((todo) => {
+    return <TodoItem task={todo.title} done={todo.done} key={todo.id} />;
+  });
   return (
     <section className="container">
       <h2>mina todos!:</h2>
-      {/* <section className="todo-section">{todoComponents}</section> */}
+      <section className="todo-section">{todoComponents}</section>
       <section className="todo-section"></section>
       <AddTodo></AddTodo>
     </section>
