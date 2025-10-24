b840c83b6ffc9d7e006c2dc50c43afcc4f6aa6a9
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-10-24 13:15:39 +0200

    added in front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,31 @@
 import "./App.css";
 import { getTodos } from "../api/api.mjs";
+import { useState, useEffect } from "react";
 import AddTodo from "./components/addtodo/addtodo.jsx";
 import TodoItem from "./components/todoItems/todoItems.jsx";
 
 function App() {
-  const todoComponents = getTodos.map((todo) => {
+  const [todos, setTodos] = useState([]); // <-- state för dina todos
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getTodos(); // <-- hämta datan
+        setTodos(data); // <-- lägg in i state
+      } catch (err) {
+        console.error("Kunde inte hämta todos:", err);
+      }
+    }
+
+    fetchData();
+  }, []); // <-- körs bara vid första renderingen
+
+  const todoComponents = todos.map((todo) => {
     return <TodoItem task={todo.title} done={todo.done} key={todo.id} />;
   });
   return (
     <section className="container">
       <h2>mina todos!:</h2>
       <section className="todo-section">{todoComponents}</section>
-      <section className="todo-section"></section>
       <AddTodo></AddTodo>
     </section>
   );
